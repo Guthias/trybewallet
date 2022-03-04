19262ae2254ed10a08f6abf0c6c4a6d409bce5fa
guthias <gmgustavom@gmail.com>
2022-03-04 18:30:48 -0300

    add: creating LoginForm structure

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -3,7 +3,23 @@ import React, { Component } from 'react';
 export default class LoginForm extends Component {
   render() {
     return (
-      <div>LoginForm</div>
+      <div>
+        <form>
+          <label htmlFor="email-input">
+            <span>Email</span>
+            <input id="email-input" data-testid="email-input" />
+          </label>
+
+          <label htmlFor="password-input">
+            <span>Password</span>
+            <input id="password-input" data-testid="password-input" />
+          </label>
+
+          <div className="login-button-area">
+            <button type="submit">Entrar</button>
+          </div>
+        </form>
+      </div>
     );
   }
 }
