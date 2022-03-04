5dd99afba27ac6cc30680c44f4f8de884d68b316
guthias <gmgustavom@gmail.com>
2022-03-04 18:35:18 -0300

    feat: connecting Login Form inputs with component state

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
 
 export default class LoginForm extends Component {
+  state = {
+    email: '',
+    password: '',
+    validForm: false,
+  }
+
   render() {
+    const { email, password, validForm } = this.state;
     return (
-      <div>
+      <main>
         <form>
           <label htmlFor="email-input">
             <span>Email</span>
-            <input id="email-input" data-testid="email-input" />
+            <input
+              id="email-input"
+              data-testid="email-input"
+              type="text"
+              value={ email }
+            />
           </label>
 
           <label htmlFor="password-input">
             <span>Password</span>
-            <input id="password-input" data-testid="password-input" />
+            <input
+              id="password-input"
+              data-testid="password-input"
+              type="password"
+              value={ password }
+            />
           </label>
 
           <div className="login-button-area">
-            <button type="submit">Entrar</button>
+            <button type="submit" disabled={ !validForm }>Entrar</button>
           </div>
         </form>
-      </div>
+      </main>
     );
   }
 }
