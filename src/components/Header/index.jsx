67f5d07a09c553d2c702d947ceaad134f344cdaf
guthias <gmgustavom@gmail.com>
2022-03-07 16:22:53 -0300

    add: creating Header HTML structure

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,20 @@ import React, { Component } from 'react';
 export default class Header extends Component {
   render() {
     return (
-      <header>Header</header>
+      <header>
+        <div>
+          <h1>TrybeWallet</h1>
+        </div>
+        <div>
+          <span data-testid="email-field">{ email }</span>
+        </div>
+        <div>
+          <span>Despesas totais: </span>
+          <span data-testid="header-currency-field">{ currency }</span>
+          &nbsp
+          <span data-testid="total-field">{ expenses.toFixed(2) }</span>
+        </div>
+      </header>
     );
   }
 }
