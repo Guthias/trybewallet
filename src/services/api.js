a22e12b722ccd23df86610e165737d5dc4aab047
unknown <gmgustavom@gmail.com>
2022-03-13 23:57:10 -0300

    feat: removing USDT from currency list

--- src/services/api.js
+++ src/services/api.js
@@ -8,5 +8,5 @@ export const getCurrencies = async () => {
   const url = 'https://economia.awesomeapi.com.br/json/all';
   const response = await fetch(url);
   const results = await response.json();
-  return Object.keys(results);
+  return Object.keys(results).filter((currency) => currency !== 'USDT');
 };
