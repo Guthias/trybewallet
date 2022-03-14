1092f192e347616588781520d6e7041c7cea1b3d
unknown <gmgustavom@gmail.com>
2022-03-14 12:52:36 -0300

    feat: creating table header  structure

--- src/components/ExpenseTable/index.jsx
+++ src/components/ExpenseTable/index.jsx
@@ -3,7 +3,21 @@ import React, { Component } from 'react';
 export default class ExpenseTable extends Component {
   render() {
     return (
-      <div>ExpenseTable</div>
+      <div>
+        <table>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </table>
+      </div>
     );
   }
 }
